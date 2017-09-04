0c4c5a9413a0230ecbcce000557a2154d2cadfa1
Sergiy Kuzmenko <s.kuzmenko@gmail.com>
2017-09-04 13:58:27 -0400

    add magnitude_to_sym test

--- spec/earthquakes_spec.rb
+++ spec/earthquakes_spec.rb
@@ -56,4 +56,20 @@ describe Earthquakes do
       expect(result).to match(properties1['title'])
     end
   end
+
+  describe 'magnitude_to_sym' do
+    it 'verifies magnitudes' do
+      expected = {
+        minor: 3.9,
+        light: 4.9,
+        moderate: 5.9,
+        strong: 6.9,
+        major: 7.9,
+        great: 8.0
+      }
+      expected.each do |sym, mag|
+        expect(Earthquakes.magnitude_to_sym(mag)).to be(sym)
+      end
+    end
+  end
 end
